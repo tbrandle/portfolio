f4a9b11233da1d02d5e51b3f080e73788d4edece
Tim <tbrandle53@gmail.com>
2017-07-11 14:55:18 -0600

    WIP About me

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ $('.arrow').on('click', function () {
   }, 800)
 })
 
-
-
 $('#nav-icon, .nav-link').click(function(){
 	$('#nav-icon').toggleClass('open');
-
-  // $('.nav-menu').fadeIn(175)
-
   $('.nav-menu').toggleClass('hidden')
 });
 
-$('.intro').mousemove(function(e) {
-    var x = (e.pageX * -1 / 60), y = (e.pageY * -1 / 60);
-    $(this).css('background-position', x + 'px ' + y + 'px');
+$(document).on('scroll', function () {
+  let currScrollPos = $(document).scrollTop();
+  $('.intro').css('background-position', '0' + -currScrollPos/4 + 'px')
 });
+
+
+
+
+// $('.intro').mousemove(function(e) {
+//     var x = (e.pageX * -1 / 60), y = (e.pageY * -1 / 60);
+//     $(this).css('background-position', x + 'px ' + y + 'px');
+// });

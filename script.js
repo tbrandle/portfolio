be873c365f6effb85c47149deb3fae7d6eb0f8c7
Tim <tbrandle53@gmail.com>
2017-07-11 10:45:22 -0600

    Add paralax image

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$('.arrow').on('click', function () {
+  $('html, body').animate({
+    scrollTop: $('#about-me').offset().top
+  }, 800)
+})
+
+
+
+$('#nav-icon, .nav-link').click(function(){
+	$('#nav-icon').toggleClass('open');
+
+  // $('.nav-menu').fadeIn(175)
+
+  $('.nav-menu').toggleClass('hidden')
+});
+
+$('.intro').mousemove(function(e) {
+    var x = (e.pageX * -1 / 60), y = (e.pageY * -1 / 60);
+    $(this).css('background-position', x + 'px ' + y + 'px');
+});
